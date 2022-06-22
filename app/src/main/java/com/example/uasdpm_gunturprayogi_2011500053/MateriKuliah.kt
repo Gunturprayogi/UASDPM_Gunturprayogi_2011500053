c1b9899f6ab4f6848c6d1ee9add48b95bbef7edc
Gunturprayogi <budubagakge@gmail.com>
2022-06-22 16:22:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uasdpm_gunturprayogi_2011500053/MateriKuliah.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm_gunturprayogi_2011500053
+
+class MateriKuliah (
+    var NIDN: String,
+    var NmDosen: String,
+    var Jabatan: String,
+    var Golongan: String,
+    var BidangKeahlian: String,
+    var ProgramStudi: String,
+    var PendidikanTerakhir: String
+)
